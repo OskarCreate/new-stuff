9aae6fba0e4d3cb07c7e541b95112a005eef7bb5
OskarCreate <oskar_morales_f@outlook.com>
2025-06-23 16:02:35 -0500

    changes of the bird

--- Assets/birdscript.cs
+++ Assets/birdscript.cs
@@ -1,16 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // NUEVO: importar el nuevo Input System
 
 public class birdscript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        gameObject.name = "Pete Bird";
-    }
+    public Rigidbody2D myRigidbody;
+    public float flatStrength;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // NUEVO: usar el nuevo sistema de Input
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flatStrength;
+        }
     }
 }
+
